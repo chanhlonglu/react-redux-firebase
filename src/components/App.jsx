9f67843c9e4649a0109d2f61e332e82ecadabde4
Chanh Long Lu <7684230+chanhlonglu@users.noreply.github.com>
2019-09-07 16:03:42 +1200

    Add App, Home and Posts components.

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Home from "components/Home";
+import Posts from "components/Posts";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/posts" component={Posts} />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Posts extends React.Component {
+  render() {
+    return <div>this is posts index</div>;
+  }
+}
+export default Posts;
