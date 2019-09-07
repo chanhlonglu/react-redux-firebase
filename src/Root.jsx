69a9be0b51bc00fae69e118c6aa187e07fcc51c2
Chanh Long Lu <7684230+chanhlonglu@users.noreply.github.com>
2019-09-07 16:51:45 +1200

    Connect Root component to redux store.

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "reducers";
+import reduxThunk from "redux-thunk";
+import reduxLogger from "redux-logger";
 
 export default ({ children, initialState = {} }) => {
-  return <div>{children}</div>;
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(reduxThunk, reduxLogger)
+  );
+
+  return <Provider store={store}>{children}</Provider>;
 };
